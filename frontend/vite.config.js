18d6e32352eb64bdfaf7b74f6eadb7d56930c1a7
Humza987 <humzainam@gmail.com>
2025-05-04 20:20:49 -0400

    minor-updates

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { loadEnv } from 'vite'
 
-//https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:4000',
-        changeOrigin: true,
-        secure: false,
-        ws: true,
-      }
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current directory
+  const env = loadEnv(mode, process.cwd())
+  
+  return {
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_REACT_APP_BACKEND_URL,
+          changeOrigin: true,
+          secure: false,
+          ws: true,
+        }
+      },
     },
-  },
-
-  plugins: [react()],
+    plugins: [react()],
+  }
 })
-
